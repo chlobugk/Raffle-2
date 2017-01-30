fd18ac054eebe728d88c63ee58ce4a329763f1f6
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-01-30 11:49:59 -0500

    pass by counter

--- raffle_winner.rb
+++ raffle_winner.rb
@@ -1,6 +1,15 @@
 def raffle_function(num, win)
 	
-	if win.include?(num)
+	counter = 0
+	win.each do |winning_ticket|
+
+		if num == winning_ticket
+			counter = counter + 1
+		end
+
+	end
+
+	if counter > 0
 		true
 	else
 		false
